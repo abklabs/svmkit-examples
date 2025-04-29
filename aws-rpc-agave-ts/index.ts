a353e2afec1adad2da837e25b19ddfd69f702d7c
Lucas Carrizo <lucas@abklabs.com>
2025-04-29 11:01:28 -0300

    add rpc agave

---
+++ aws-rpc-agave-ts/index.ts
@@ -0,0 +1,105 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as svmkit from "@svmkit/pulumi-svmkit";
+
+const rpcConfig = new pulumi.Config("rpc");
+const solanaConfig = new pulumi.Config("solana");
+const tunerConfig = new pulumi.Config("tuner");
+
+// AWS-specific resources are created inside.
+import { sshKey, instance } from "./aws";
+
+// Lookup information about the Solana network.
+const networkName =
+  solanaConfig.get<svmkit.solana.NetworkName>("network") ??
+  svmkit.solana.NetworkName.Devnet;
+const networkInfo = svmkit.networkinfo.getNetworkInfoOutput({ networkName });
+const agaveVersion = rpcConfig.get("version") ?? "2.1.13-1";
+
+// Create some keys for this rpc to use.
+const rpcKey = new svmkit.KeyPair("rpc-key");
+
+// Point pulumi-svmkit at the AWS EC2 instance's SSH connection.
+const connection = {
+  host: instance.publicDns,
+  user: "admin",
+  privateKey: sshKey.privateKeyOpenssh,
+};
+
+// Tuner setup
+const tunerVariant =
+    tunerConfig.get<svmkit.tuner.TunerVariant>("variant") ??
+    svmkit.tuner.TunerVariant.Generic;
+
+// Retrieve the default tuner parameters for that variant
+const genericTunerParamsOutput = svmkit.tuner.getDefaultTunerParamsOutput({
+  variant: tunerVariant,
+});
+
+// "Apply" those params so we can pass them to the Tuner constructor
+const tunerParams = genericTunerParamsOutput.apply((p) => ({
+  cpuGovernor: p.cpuGovernor,
+  kernel: p.kernel,
+  net: p.net,
+  vm: p.vm,
+  fs: p.fs,
+}));
+
+
+// Create the Tuner resource on the EC2 instance
+const tuner = new svmkit.tuner.Tuner(
+  "tuner",
+  {
+    connection,
+    params: tunerParams,
+  },
+  {
+    dependsOn: [instance],
+  }
+);
+
+// Instantiate a new Agave instance on the machine.
+new svmkit.rcp.Agave(
+  "rpc-server",
+  {
+    connection,
+    version: agaveVersion,
+    environment: {
+      rpcURL: networkInfo.rpcURL[0],
+    },
+    keyPairs: {
+      identity: rpcKey.json,
+    },
+    flags: {
+      useSnapshotArchivesAtStartup: "when-newest",
+      fullRpcAPI: true,
+      rpcPort: 8899,
+      privateRPC: false,
+      onlyKnownRPC: false,
+      dynamicPortRange: "8002-8020",
+      gossipPort: 8001,
+      rpcBindAddress: "0.0.0.0",
+      walRecoveryMode: "skip_any_corrupted_record",
+      limitLedgerSize: networkName === "mainnet" ? 100000000 : 50000000,
+      blockProductionMethod: "central-scheduler",
+      fullSnapshotIntervalSlots: 1000,
+      noWaitForVoteToStartLeader: true,
+      noVoting: true,
+      accountsIndexProofSize: 2048,
+      accountsIndexMemoryLimit: networkName === "mainnet" ? 300000 : 100000,
+      accountsIndexSkipShrink: true,
+      accountsDbCachingEnabled: true,
+      entryPoint: networkInfo.entryPoint,
+      knownValidator: networkInfo.knownValidator,
+      expectedGenesisHash: networkInfo.genesisHash,
+    },
+  },
+  {
+    dependsOn: [instance],
+  },
+);
+
+// Expose information required to SSH to the rpc host.
+export const nodes_name = ["rpc-instance"];
+export const nodes_public_ip = [instance.publicIp];
+export const nodes_private_key = [sshKey.privateKeyOpenssh];
+export const tuner_params = tunerParams;
