5bee623c5209dc31912b757cec16f8a41fa7c35b
Dante DeLucia <dante@abklabs.com>
2025-07-11 20:14:16 -0700

    Formatting cleanups

--- .buildkite/cloud-creds.sh
+++ .buildkite/cloud-creds.sh
@@ -7,21 +7,20 @@ echo "INFO: Setting up environment for cloud: ${CLOUD:-unspecified}"
 export PULUMI_ACCESS_TOKEN="$(buildkite-agent secret get PULUMI_ACCESS_TOKEN)"
 
 case "${CLOUD:-}" in
-  aws)
-    export AWS_ACCESS_KEY_ID="$(buildkite-agent secret get AWS_ACCESS_KEY_ID)"
-    export AWS_SECRET_ACCESS_KEY="$(buildkite-agent secret get AWS_SECRET_ACCESS_KEY)"
-    ;;
+aws)
+	export AWS_ACCESS_KEY_ID="$(buildkite-agent secret get AWS_ACCESS_KEY_ID)"
+	export AWS_SECRET_ACCESS_KEY="$(buildkite-agent secret get AWS_SECRET_ACCESS_KEY)"
+	;;
 
-  gcp)
-      : "${GOOGLE_PROJECT:=svmkit}"
-      : "${GOOGLE_REGION:=us-central1}"
-      : "${GOOGLE_ZONE:=${GOOGLE_REGION}-a}"
-      
-    ;;
+gcp)
+	: "${GOOGLE_PROJECT:=svmkit}"
+	: "${GOOGLE_REGION:=us-central1}"
+	: "${GOOGLE_ZONE:=${GOOGLE_REGION}-a}"
 
-  *)
-    echo "ERROR: Unsupported or missing CLOUD environment variable: '${CLOUD:-}'"
-    exit 1
-    ;;
+	;;
+
+*)
+	echo "ERROR: Unsupported or missing CLOUD environment variable: '${CLOUD:-}'"
+	exit 1
+	;;
 esac
-
